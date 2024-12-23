ef38e2f9fbe5160855801d0b868ff89552c9c3ce
Zlatanova Irina <t0ffy@ya.ru>
2024-12-24 00:26:19 +0200

    refactor: test new solution

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,5 +1,35 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
+/** **/
+
+/**
+ * Given two strings, find the number of common characters between them.
+ *
+ * @param {String} s1
+ * @param {String} s2
+ * @return {Number}
+ *
+ * @example
+ * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
+ * Strings have 3 common characters - 2 "a"s and 1 "c".
+ */
+
+// function getCommonCharacterCount(s1, s2) {
+
+//   const regexp = new RegExp(`[${s2.split('').join('*')}]`, 'g');
+
+//   console.log('s1: ', s1);
+//   console.log('s2: ', s2);
+
+//   console.log('mtch: ', [...s1.matchAll(regexp)]);
+  
+
+
+//   return [...s1.matchAll(regexp)].length;
+// }
+
+//  *First solution
+
 const getCharsCountMap = (str) => {
   return [...str].reduce((charCountMap, char) => {
     if (charCountMap.has(char)) {
@@ -11,12 +41,6 @@ const getCharsCountMap = (str) => {
   }, new Map());
 }
 
-// const counter = () => {
-//   let count = 0;
-//   return function inc (increment = 1) {
-//     count += increment;
-//   }
-// }
 /**
  * Given two strings, find the number of common characters between them.
  *
@@ -46,6 +70,7 @@ function getCommonCharacterCount(s1, s2) {
 
   return result;
 }
+/********************************/
 
 module.exports = {
   getCommonCharacterCount
