978c63f17c972388fb99c5ea0f9f6dbe7be9ecb1
Zlatanova Irina <t0ffy@ya.ru>
2024-12-25 11:35:29 +0200

    feat: implement createDreamTeam in dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  
+  return members
+    .filter((memberName) => typeof memberName === 'string' && memberName.length > 0)
+    .map((memberName) => memberName.trim()[0].toUpperCase())
+    .sort()
+    .join('');
+
 }
 
 module.exports = {
