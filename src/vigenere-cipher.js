22b79cf8e32c7aeee58eaf9e5f7fbfbdbd2b77f6
Zlatanova Irina <t0ffy@ya.ru>
2025-01-05 00:45:49 +0200

    feat: implement VigenereCipheringMachine class in vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,95 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  alphabet = "abcdefghijklmnopqrstuvwxyz";
+  alphabetCharObject = [...this.alphabet.toUpperCase()].reduce((obj, currentChar, index, array) => {
+    obj[currentChar] = index;
+    return obj;
+  }, {});
+  alphabetIndexObject = [...this.alphabet.toUpperCase()].reduce((obj, currentChar, index, array) => {
+    obj[index] = currentChar;
+    return obj;
+  }, {});
+
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  static checkArgs(args) {
+    if ([...args].some(($) => !$) || args.length < 2) {
+      throw new Error('Incorrect arguments!');
+    }
+  }
+
+  static getPaddedForLengthKey(key, length) {
+    while(key.length < length) {
+      key = key.repeat(2);
+    }
+    return key.slice(0, length);
+  }
+
+  encrypt(message, key) {
+
+    VigenereCipheringMachine.checkArgs(arguments);
+
+    const messageUpperCase = message.toUpperCase();
+    const paddedKey = VigenereCipheringMachine.getPaddedForLengthKey(key, messageUpperCase.length);
+    const keyUpperCase = paddedKey.toUpperCase();
+
+    let keyUpperCaseIndex = 0;
+
+    let result = [...messageUpperCase]
+      .map((char) => {
+ 
+        if (!(char in this.alphabetCharObject)) {
+          return char;
+        }
+
+        const encryptedCharIndex = (this.alphabetCharObject[char] + this.alphabetCharObject[keyUpperCase[keyUpperCaseIndex]]) % 26;
+
+        keyUpperCaseIndex += 1;
+
+        return this.alphabetIndexObject[encryptedCharIndex];
+      })
+      .join('');
+
+      if (!this.direct) {
+        result = result.split('').reverse().join('');
+      }
+
+      return result;
+  }
+
+  decrypt(encryptedMessage, key) {
+    VigenereCipheringMachine.checkArgs(arguments);
+
+    const messageUpperCase = encryptedMessage.toUpperCase();
+    const paddedKey = VigenereCipheringMachine.getPaddedForLengthKey(key, messageUpperCase.length);
+    const keyUpperCase = paddedKey.toUpperCase();
+
+    let keyUpperCaseIndex = 0;
+
+    let result = [...messageUpperCase]
+      .map((char) => {
+ 
+        if (!(char in this.alphabetCharObject)) {
+          return char;
+        }
+
+        const encryptedCharIndex = Math.abs((this.alphabetCharObject[char] - this.alphabetCharObject[keyUpperCase[keyUpperCaseIndex]] + 26) % 26);
+
+        keyUpperCaseIndex += 1;
+
+        return this.alphabetIndexObject[encryptedCharIndex];
+      })
+      .join('');
+
+      if (!this.direct) {
+        result = result.split('').reverse().join('');
+      }
+
+      return result;
   }
 }
 
